9edaeb983bd055989b6efbc85bf8c785b236a689
Uxue <thechuchu049@gmail.com>
2024-07-06 14:04:38 -0600

    Insert title page

--- hacktion.py
+++ hacktion.py
@@ -1,5 +1,13 @@
 import streamlit as st 
 
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Listo para mitigar riesgos?? ⚠ ⚠")
+
 st.write(""" 
 # pruebasasknHola
 asdasdasdasd.
